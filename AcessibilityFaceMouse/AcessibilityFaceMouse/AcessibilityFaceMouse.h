5bb7d01173340da80b20946bab3cca3657d73090
João Batista <joao.damasceno@bs2tecnologia.com.br>
2021-03-03 18:30:30 -0300

    adicionado framework

---
+++ AcessibilityFaceMouse/AcessibilityFaceMouse/AcessibilityFaceMouse.h
@@ -0,0 +1,19 @@
+//
+//  AcessibilityFaceMouse.h
+//  AcessibilityFaceMouse
+//
+//  Created by Joao Batista on 03/03/21.
+//  Copyright © 2021 Joao Batista. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AcessibilityFaceMouse.
+FOUNDATION_EXPORT double AcessibilityFaceMouseVersionNumber;
+
+//! Project version string for AcessibilityFaceMouse.
+FOUNDATION_EXPORT const unsigned char AcessibilityFaceMouseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AcessibilityFaceMouse/PublicHeader.h>
+
+
